a09c14a0d441df06554c38ddae23a447a72f40bc
Joel Matthys <jwmatthys@yahoo.com>
2013-03-04 01:15:58 -0500

    mix source audio to mono

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=1;
 for file in *.wav;
-do ffmpeg -i "$file" -ar 44100 $i.wav; i=`expr $i + 1`;
+do ffmpeg -i "$file" -ac 1 -ar 44100 $i.wav; i=`expr $i + 1`;
 done
 normalize-audio *.wav
