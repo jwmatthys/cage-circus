62c1b2e18d4a3fea9a4d2410759e2759fd988f0e
Joel Matthys <jwmatthys@yahoo.com>
2013-03-07 02:04:08 -0500

    revert

--- lauren.ck
+++ lauren.ck
@@ -38,7 +38,7 @@ setFiles_Per_Min();
 SndBuf temp => blackhole;
 WvOut2 out;
 
-me.sourceDir() + "/data/" => string path;
+me.sourceDir() + "/" => string path;
 
 if (recording)
 {
--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=1;
-for file in orig_audio/*.wav;
-do ffmpeg -i "$file" -ac 1 -ar 44100 ../data/$i.wav; i=`expr $i + 1`;
+for file in *.wav;
+do ffmpeg -i "$file" -ac 1 -ar 44100 $i.wav; i=`expr $i + 1`;
 done
-normalize-audio data/*.wav
+normalize-audio *.wav
